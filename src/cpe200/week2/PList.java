b0a953f8c87a37e53d1481c003b7e832a86a7121
supitch <sudukutoto@hotmail.com>
2016-09-06 11:27:24 +0700

    HW-2 done

--- src/cpe200/week2/PList.java
+++ src/cpe200/week2/PList.java
@@ -14,7 +14,13 @@ public class PList {
     }
 
     public void pushToTail(char i) {
-        // implement your code here
+        PNode tmp = new PNode();
+        tmp.data = i;
+        tmp.next=null;
+        tail.next = tmp;
+        tmp.prev = tail;
+        tail=tmp;
+        size++;
     }
 
     public char popHead() {
@@ -35,11 +41,26 @@ public class PList {
     }
 
     public char popTail() {
-        // implement your code here
+        char temp = tail.data;
+        if(head==tail)
+            head=tail=null;
+        else
+            {tail=tail.prev;
+                tail.next = null;
+                size--;
+            }
+                return temp;
     }
 
     public boolean search(char i) {
-        // implement your code here
+        PNode temp = head;
+        while(temp!=null)
+            {
+                if(temp.data == i)
+                    return true;
+                temp = temp.next;
+            }
+            return false;
     }
 
     public boolean isEmpty() { return (head == null); }
@@ -48,13 +69,21 @@ public class PList {
         PNode tmp = head;
 
         while (tmp != null) {
-            // implement your code here
+            System.out.print(tmp.data);
+            tmp = tmp.next;
         }
         System.out.println();
     }
 
     public void printBackward() {
-        // implement your code here
+        PNode tmp = tail;
+        while(tmp!=head)
+            {
+                System.out.print(tmp.data);
+                tmp = tmp.prev;
+            }
+            if(tmp!=null)
+                System.out.println(tmp.data);
     }
 
     public int getSize() {
